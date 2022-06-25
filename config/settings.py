b130bf95ee576a880160bf9eba64d88cdc2585f2
Abuka-Victor <abukavictoro@gmail.com>
2022-06-25 23:55:53 +0100

    Set Debug to False

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^@ipm_4!lqt4w@8l-3qglgg0)+arwoowgb12n&guhljwy(=+b8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
