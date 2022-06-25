acffc8865f5198d8448805c770f0583a987869ea
Abuka-Victor <abukavictoro@gmail.com>
2022-06-25 22:29:40 +0100

    Added Create Feature

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, contact_detail, contact_delete
+from .views import index, contact_detail, contact_delete, contact_create
 
 app_name = 'pb'
 
 urlpatterns = [
     path('', index, name="home"),
+    path('create/', contact_create, name="create"),
     path('<int:id>/', contact_detail, name="detail"),
-    path('delete/<int:id>/', contact_delete, name='delete')
+    path('delete/<int:id>/', contact_delete, name='delete'),
 ]
--- phonebook/views.py
+++ phonebook/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Contact
-
+from django.contrib import messages
 
 def index(request):
     contacts = Contact.objects.all()
@@ -22,3 +22,20 @@ def contact_delete(request, id):
     contact = Contact.objects.get(id=id)
     contact.delete()
     return redirect("/")
+
+def contact_create(request):
+    if request.method == "POST":
+        first_name = request.POST['first_name'] if request.POST['first_name'] else None
+        last_name = request.POST['last_name'] if request.POST['last_name'] else None
+        phone_number = request.POST['phone_number'] if request.POST['phone_number'] else None
+        if None not in [first_name, last_name, phone_number]:
+            Contact.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                phone_number=phone_number)
+        else:
+            messages.error(request, "All Fields Should Be Filled")
+            return redirect("/create")
+        return redirect("/")
+    elif request.method == "GET":
+        return render(request, "phonebook/contact_create.html")
