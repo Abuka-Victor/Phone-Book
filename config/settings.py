4a84c61dacc635fbfdcd1c8e75d063b40022c42a
Abuka-Victor <abukavictoro@gmail.com>
2022-06-26 14:51:49 +0100

    Set Debug True, for Static File Sake

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^@ipm_4!lqt4w@8l-3qglgg0)+arwoowgb12n&guhljwy(=+b8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
